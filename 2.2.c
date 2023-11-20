3bcedcc77978b7c5ca50195dde20af427dacacf1
mohak-borole <mohakborole@gmail.com>
2023-11-20 18:52:36 +0530

    Updated @

--- 2.2.c
+++ 2.2.c
@@ -1,9 +1,20 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
 
 int main()
 {
-    printf("Before\n");
-    execl("/bin/ls" , "ls" , "-l" , NULL);
-    printf("After\n");
+    pid_t q;
+    q = fork();
+    if(q = 0)
+    {
+        printf("I am child process having ID: %d\n" , getpid());
+        execl("/bin/ps" , "ps" , NULL);
+    }
+    else
+     {
+        wait(NULL);
+        printf("I am parent process having ID: %d\n" , getpid());
+    }
 }
